e7c109f49b561c77cc96ddcb3eaa22a566efaa9e
kirimanjyaro <eryuchunliang@gmail.com>
2024-06-13 01:19:04 +0900

    [update]omikuji.jsにrandam関数におけるif分を追記

--- omikuji.js
+++ omikuji.js
@@ -3,3 +3,6 @@ let userresult;
 username = prompt("お名前を教えて下さい。");
 document.getElementById("name").innerHTML = username;
 let rand = Math.floor( Math.random() * 5) ;
+if (rand == 0) {
+  userresult = "大吉";
+}
